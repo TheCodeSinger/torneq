7f699f70346af0bebdaf564dd38e53ced1907cc7
Timothy <n1029676@gmail.com>
2019-08-15 00:26:59 -0700

    Added basic html view of results and abstracted async job builder

--- app/targets/urls.py
+++ app/targets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('json', views.targets_json_async, name='API Results'),
+    path('view/<int:minStats>', views.targets_pretty, name='Functional View')
 ]
--- app/targets/views.py
+++ app/targets/views.py
@@ -6,6 +6,20 @@ from keymanager import models as kmodels
 from celery import group
 
 
+def _async_stat_updates_(minStats: int, targetCount: int = 8):
+    accounts = kmodels.Account.objects.filter(api_ready=True)
+    spy_reports = tmodels.SpyReport.objects.filter(total__gte=minStats, archived=False) \
+                      .order_by('total', '-date_spied') \
+                      .select_related('torn_id')[:min(targetCount, settings.TORN_API_MAX_TARGET_RETURN)]
+    tmpjobs = list()
+    for index, report in enumerate(spy_reports):
+        # print(index, index % len(accounts), 'updated', report.torn_id, sep='\t')
+        tmpjobs.append(
+            tmodels.update_profile_job.s(target_pk=report.torn_id_id, account_pk=accounts[index % len(accounts)].pk))
+    batchresult = group(tmpjobs).apply_async()
+    return batchresult
+
+
 def targets_json_async(request):
     try:
         minStats = int(request.GET.get('minStats', 100))
@@ -17,13 +31,15 @@ def targets_json_async(request):
     except ValueError:
         targetCount = 8
 
-    accounts = kmodels.Account.objects.filter(api_ready=True)
-    spy_reports = tmodels.SpyReport.objects.filter(total__gte=minStats, archived=False)\
-        .order_by('total', '-date_spied')\
-        .select_related('torn_id')[:min(targetCount, settings.TORN_API_MAX_TARGET_RETURN)]
-    tmpjobs = list()
-    for index, report in enumerate(spy_reports):
-        # print(index, index % len(accounts), 'updated', report.torn_id, sep='\t')
-        tmpjobs.append(tmodels.update_profile_job.s(target_pk=report.torn_id_id, account_pk=accounts[index%len(accounts)].pk))
-    batchresult = group(tmpjobs).apply_async()
-    return JsonResponse({'targets': batchresult.join()}, json_dumps_params={'indent': 2})
+    results = _async_stat_updates_(minStats=minStats, targetCount=targetCount)
+    return JsonResponse({'targets': results.join()}, json_dumps_params={'indent': 2})
+
+
+def targets_pretty(request, minStats=100):
+    try:
+        minStats = int(minStats)
+    except ValueError:
+        minStats = 100
+
+    results = _async_stat_updates_(minStats=minStats)
+    return render(request, 'targets/list.html', {'targets': results.join()})
