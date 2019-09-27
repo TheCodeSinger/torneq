de986980ac9b0490cbe1abee6e19bf198de574dd
Timothy <n1029676@gmail.com>
2019-09-26 23:13:27 -0700

    Properly accept application/json data from webapp

--- app/keymanager/views.py
+++ app/keymanager/views.py
@@ -7,15 +7,17 @@ from django.views.decorators.csrf import csrf_exempt
 from factionstats import settings
 from . import models as kmodels
 import requests
+import json
 
 
 @csrf_exempt
 @require_http_methods(['POST'])
 def tornauth(request):
-    if 'apikey' in request.POST:
-        if 13 < len(request.POST['apikey']) < 23:
+    user_apikey = json.loads(request.body).get('apikey')
+    if user_apikey:
+        if 13 < len(user_apikey) < 23:
             results = requests.get(
-                url=f"""{settings.TORN_API_BASE_URL}user/?selections=profile&key={request.POST['apikey']}""")
+                url=f"""{settings.TORN_API_BASE_URL}user/?selections=profile&key={user_apikey}""")
             if 'error' in results.json():
                 return JsonResponse(data=results.json())
             elif 'player_id' in results.json():
@@ -23,7 +25,7 @@ def tornauth(request):
                     login_user = User.objects.get_or_create(username=results.json()['name'])
                     api_account = kmodels.Account.objects.get_or_create(torn_id=results.json()['player_id'],
                                                                         torn_name=results.json()['name'],
-                                                                        api_key=request.POST['apikey'],
+                                                                        api_key=user_apikey,
                                                                         fsuser_id=login_user[0])
                     auth.login(request, login_user[0])
                     return JsonResponse(data={'login': True, 'name': login_user[0].username, 'tornid': api_account[0].torn_id})
