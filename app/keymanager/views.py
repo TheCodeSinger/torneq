d6380e736986a14523cefa6def89f0c930e8cd09
Timothy <n1029676@gmail.com>
2019-11-24 23:38:04 -0800

    Double check Key is valid...

--- app/keymanager/views.py
+++ app/keymanager/views.py
@@ -28,8 +28,9 @@ def tornauth(request):
                                                                         torn_name=results.json()['name'],
                                                                         api_key=user_apikey,
                                                                         fsuser_id=login_user[0])
-                    auth.login(request, login_user[0])
-                    return JsonResponse(data={'login': True, 'name': login_user[0].username, 'tornid': api_account[0].torn_id})
+                    if api_account[0].test_key_validity():
+                        auth.login(request, login_user[0])
+                        return JsonResponse(data={'login': True, 'name': login_user[0].username, 'tornid': api_account[0].torn_id})
                 else:
                     return JsonResponse(data={'login': False, 'error': 'Not in the righteous faction.'})
 
